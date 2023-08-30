1d52bec73dd32f56316017891e65919d402e0466
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2023-08-30 23:37:30 +0530

    added codes

---
+++ lib/welcomepage.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+class Welcomepage extends StatefulWidget{
+
+  @override
+  _WelcomepageState createState()=> _WelcomepageState();
+}
+
+
+class _WelcomepageState extends State<Welcomepage>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
