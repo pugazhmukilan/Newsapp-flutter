4509df69a76bbaa696211a576464cf7b6c6b6582
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2023-08-31 10:06:40 +0530

    third commit

---
+++ lib/Signup.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+import "constants.dart";
+
+
+class Signup extends StatefulWidget{
+
+  @override
+  
+  _Signup createState() => _Signup();
+}
+
+class _Signup extends State<Signup>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      backgroundColor: kBackgroundcolor,
+
+    );
+  }
+}
--- lib/welcomepage.dart
+++ lib/welcomepage.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 
+import 'Signup.dart';
 import "constants.dart";
+import "login.dart";
 class Welcomepage extends StatefulWidget{
 
   @override
@@ -27,7 +29,7 @@ class _WelcomepageState extends State<Welcomepage>{
               width: 400,
               
               decoration: ShapeDecoration(
-                color: Color(0xFFD9D9D9),
+                color: Color.fromARGB(255, 134, 134, 134),
                 shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(50),
@@ -35,12 +37,58 @@ class _WelcomepageState extends State<Welcomepage>{
                 ),
                 ),
                 ),
-                            
+              child:Padding(
+                padding: const EdgeInsets.only(left: 50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomButton(onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>Login()));
+                    }, colours: const Color.fromARGB(255, 235, 235, 235)
+                    , text: "Login"),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    CustomButton(onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>Signup()));
+                    }, colours: const Color.fromARGB(255, 235, 235, 235)
+                    , text: "Signup"),
+                  ],
+                ),
+              ),
             
                   ),
           ),
+          
         ],
     
+      ),
+    );
+  }
+}
+
+
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Color colours;
+  final String text;
+
+  CustomButton({required this.onPressed,required this.colours,required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 3),
+      child: SizedBox(
+        height: 60,
+        width: 250,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: colours,shadowColor: Colors.black),
+          onPressed: onPressed,
+          child: Text(text),
+        ),
       ),
     );
   }
