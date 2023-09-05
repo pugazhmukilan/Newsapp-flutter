2d4ebf5d02a5ab2f05b59154f4caf86fe5ad9adf
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2023-09-05 14:21:53 +0530

    interest page

--- lib/interest.dart
+++ lib/interest.dart
@@ -19,6 +19,16 @@ class _InterestState extends State<Interest>{
             Expanded(
               flex:1,
               child: SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[ Center(child: Text("Interest",style:TextStyle(fontFamily: "Pacifico",
+                  fontSize: 60,
+                  color: Colors.white),)),
+
+                  Text("choose your intrested topics for the news feed",style:TextStyle(fontFamily:"Commissioner",
+                  fontSize: 12,
+                  color: Color.fromARGB(255, 137, 137, 137) )),
+           ] ),
                 height: 80,
               ),
             ),
