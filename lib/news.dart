b3ed18f5319c66f1825cf617eb748eb9285d0cc5
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2023-10-29 21:49:00 +0530

    new api

--- lib/news.dart
+++ lib/news.dart
@@ -1,37 +1,33 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import "package:flutter/material.dart";
-import "package:flutter_swiper_view/flutter_swiper_view.dart";
-import "package:news_app/constants.dart";
+import 'package:flutter/material.dart';
 
-import "interest.dart";
-import "welcomepage.dart";
-
-FirebaseAuth auth =FirebaseAuth.instance;
-class Newspage extends StatefulWidget{
-  late List<String>subtitles1;
+import "constants.dart";
+class Newspage extends StatefulWidget {
+  late List<String> subtitles1;
   Newspage({required this.subtitles1});
 
   @override
-  _NewspageState createState()=> _NewspageState();
+  _NewspageState createState() => _NewspageState();
 }
 
-class _NewspageState extends State<Newspage>{
+class _NewspageState extends State<Newspage> {
   late List<String> subtitles;
+  late String selectedCategory;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    subtitles =widget.subtitles1;
+    subtitles = widget.subtitles1;
+    selectedCategory = subtitles.first; // Set the default category
   }
-  
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "News",
           style: TextStyle(
-            color: Color.fromARGB(255, 112, 112, 112),
+            color: Color.fromARGB(255, 255, 255, 255),
             fontFamily: "Pacifico",
             fontSize: 35,
           ),
@@ -40,120 +36,120 @@ class _NewspageState extends State<Newspage>{
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.pop(context);
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Interest()));
+              // ... (your existing code for Interest page)
             },
-            icon: Icon(Icons.abc_outlined, color: Color.fromARGB(255, 112, 112, 112)),
+            icon: Icon(Icons.abc_outlined, color: Color.fromARGB(255, 255, 255, 255)),
           ),
           IconButton(
             onPressed: () {
-              FirebaseAuth auth = FirebaseAuth.instance;
-              auth.signOut().then((value) async {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Welcomepage()));
-              });
+              // ... (your existing code for logout)
             },
-            icon: Icon(Icons.logout_outlined, color: Color.fromARGB(255, 112, 112, 112)),
+            icon: Icon(Icons.logout_outlined, color: Color.fromARGB(255, 255, 255, 255)),
           ),
         ],
       ),
-
       backgroundColor: kBackgroundcolor,
-      body:Column(
+      body: Column(
         children: [
-          ButtonRow(listname: subtitles,defaultbutton: "Currency", height: 50, whenselected: const Color.fromARGB(255, 83, 83, 83), whennotselected: const Color.fromARGB(255, 43, 43, 43), textselected: Colors.white, textnotselected: Color.fromARGB(255, 136, 136, 136)),
-          Expanded(
-            child: Swiper(itemCount: subtitles.length,
-              itemBuilder: (context, index) {
-                
-                  return Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
-                    child: Container(
-                      
-                     
-                      child: CustomScrollView(
-                        physics: BouncingScrollPhysics(),
-                        slivers: [
-                          SliverList.builder(
-                            itemCount: 10,
-                            
-                            itemBuilder:(context, index) {
-                            
-                            return Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                              child: Container(
-                                height:400,
-                                decoration: BoxDecoration(
-                                   color: const Color.fromARGB(255, 3, 30, 53),
-                                   borderRadius: BorderRadius.circular(30)
-                                ),
-                               
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Column(
-                                    
-                                   
-                                    children: [
-                                    ClipRRect(
-                                    borderRadius: BorderRadius.circular(20), // Adjust the border radius as needed
-                                    child: Image.network(
-                                      "https://static.toiimg.com/thumb/imgsize-46090,msid-103973373,width-400,resizemode-4/103973373.jpg",
-                                      height: 190,
-                                    ),
-                                  ),
-                                    Text("this is the sample news for checking\n heheheheh",style: TextStyle(color: Colors.white,)),
-                                  ],),
-                                ),
-                              ),
-                            );
-                          },)
-                        ],
-                      ),
-                    ),
-                  );
-              },
-            
-            ),
+          ButtonRow(
+            listname: subtitles,
+            height: 50,
+            whenselected: const Color.fromARGB(255, 83, 83, 83),
+            whennotselected: const Color.fromARGB(255, 43, 43, 43),
+            textselected: Colors.white,
+            textnotselected: Color.fromARGB(255, 136, 136, 136),
+            defaultbutton: selectedCategory,
+            onCategorySelected: (category) {
+              setState(() {
+                selectedCategory = category;
+              });
+            },
           ),
-        
+          Expanded(
+            child: NewsList(category: selectedCategory),
+          ),
+          ElevatedButton(onPressed: (){
+            getnews();
+          }, child: Text("get the news")),
         ],
-      )
+      ),
     );
   }
 }
 
+class NewsList extends StatelessWidget {
+  final String category;
+
+  NewsList({required this.category});
+
+  // Implement your logic to fetch news based on the category and display here
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10, // Adjust the number of items based on your data
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+          child: Container(
+           
+            height:300,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network("https://static.toiimg.com/thumb/imgsize-81054,msid-104771131,width-600,resizemode-4/104771131.jpg",scale: 1.6,),
+                Expanded(
+                  child: Container(
+                    width:double.infinity,
+                    
+                    
+                    child:Center(child: Text("$category, $index",style: TextStyle(color: Colors.white),),),
+                
+                
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
 
 class ButtonRow extends StatefulWidget {
-
   late List<String> listname;
-  late  double height;
+  late double height;
   late Color whenselected;
   late Color whennotselected;
   late Color textselected;
   late Color textnotselected;
   late String defaultbutton;
-  
-  ButtonRow({required this.listname,
-  required this.height,
-  required this.whenselected,
-  required this.whennotselected,
-  required this.textselected,
-  required this.textnotselected,
-  required this.defaultbutton,
+  final Function(String) onCategorySelected;
+
+  ButtonRow({
+    required this.listname,
+    required this.height,
+    required this.whenselected,
+    required this.whennotselected,
+    required this.textselected,
+    required this.textnotselected,
+    required this.defaultbutton,
+    required this.onCategorySelected,
   });
+
   @override
   _ButtonRowState createState() => _ButtonRowState();
 }
 
 class _ButtonRowState extends State<ButtonRow> {
-  
-  late String selectedButton=widget.defaultbutton; // Default selected button
-
- 
+  late String selectedButton = widget.defaultbutton; // Default selected button
 
   void _handleButtonPress(String buttonName) {
     setState(() {
       selectedButton = buttonName;
+      widget.onCategorySelected(buttonName);
     });
   }
 
@@ -168,11 +164,10 @@ class _ButtonRowState extends State<ButtonRow> {
           return GestureDetector(
             onTap: () => _handleButtonPress(buttonName),
             child: Container(
-              height:widget.height,
+              height: widget.height,
               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
               margin: EdgeInsets.all(8),
               decoration: BoxDecoration(
-                
                 borderRadius: BorderRadius.circular(16),
                 border: Border.all(
                   color: Colors.transparent,
@@ -184,13 +179,11 @@ class _ButtonRowState extends State<ButtonRow> {
                 child: Text(
                   buttonName,
                   style: TextStyle(
-                    fontSize:15,
+                    fontSize: 15,
                     fontFamily: "Raleway",
                     fontWeight: FontWeight.w400,
                     decoration: TextDecoration.none,
-                    color: isSelected? widget.textselected:widget.textnotselected,
-                    
-              
+                    color: isSelected ? widget.textselected : widget.textnotselected,
                   ),
                 ),
               ),
@@ -201,4 +194,3 @@ class _ButtonRowState extends State<ButtonRow> {
     );
   }
 }
-
